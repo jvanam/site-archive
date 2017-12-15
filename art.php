2613c95369b80e601a45a28eefe33ec9a690b431
jvanam <jessicavanam@gmail.com>
2017-12-14 22:14:13 -0500

    delted some things

---
+++ art.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+ <meta charset="UTF-8">
+ <title>JV</title>
+ </head>
+ <body>
+<?php include("includes/header.php"); ?>
+<div id='info_title'>
+  <p class=titular_text>Art</p>
+</div> <!-- info_title -->
+<div class='sculpture'>
+  <p class='link'>Big Lips Little Nose<br></p>
+  <img class="sculpturephotofirst" src="images/biglipsright.jpg"/>
+  <img class="sculpturephoto" src="images/biglipsfront.jpg"/>
+  <img class="sculpturephoto" src="images/biglipsleft.jpg"/>
+</div> <!--scuplture-->
+<div class='scuplture'>
+  <p class='link'>Leg</p>
+  <img class="sculpturephotofirst" src="images/legleft.jpg"/>
+  <img class="sculpturephoto" src="images/legfront.jpg"/>
+  <img class="sculpturephoto" src="images/legright.jpg"/>
+</div> <!--scuplture-->
+  <p class='link'>Sleeper<br></p>
+  <img class="sculpturephotofirst" src="images/face1right.jpg"/>
+  <img class="sculpturephoto" src="images/face1front.jpg"/>
+  <img class="sculpturephoto" src="images/face1left.jpg"/>
+</div> <!--scuplture-->
+<div class='scuplture'>
+  <p class='link'>Jack (for a box)<br></p>
+  <img class="sculpturephotofirst" src="images/jackright.jpg"/>
+  <img class="sculpturephoto" src="images/jackfront.jpg"/>
+  <img class="sculpturephoto" src="images/jackleft.jpg"/>
+</div> <!--scuplture-->
+<div class='scuplture'>
+  <p class='link'>Mourning Head<br></p>
+  <img class="sculpturephotofirst" src="images/face2right.jpg"/>
+  <img class="sculpturephoto" src="images/face2front.jpg"/>
+  <img class="sculpturephoto" src="images/face2left.jpg"/>
+</div> <!--scuplture-->
+<div class='scuplture'>
+  <p class='link'>Kind Horned Guy<br></p>
+  <img class="sculpturephotofirst" src="images/face3right.jpg"/>
+  <img class="sculpturephoto" src="images/face3front.jpg"/>
+  <img class="sculpturephoto" src="images/face3left.jpg"/>
+</div> <!--scuplture-->
+<div class='scuplture'>
+  <p class='link'>Back (for a necklace)<br></p>
+  <img class="sculpturephotofirst" src="images/backnecklace.jpg"/>
+</div> <!--scuplture-->
+
+<?php include("includes/footer.php"); ?>
+ </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+	<meta charset="UTF-8">
+	<title>JV</title>
+	</head>
+	<body>
+<?php include("includes/header.php"); ?>
+<p class='link' id='resume'>RESUME</p>
+<p class='link' id='contact'>CONTACT ME</p>
+<?php include("includes/footer.php"); ?>
+	</body>
+</html>
