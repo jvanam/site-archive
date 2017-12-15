2c3d698c2a4932f5547c15ef4abf4fe2e57aefba
jvanam <32110513+jvanam@users.noreply.github.com>
2017-12-15 15:35:39 -0500

    php to html

--- art.php
+++ art.php
@@ -6,7 +6,27 @@
  <title>JV</title>
  </head>
  <body>
-<?php include("includes/header.php"); ?>
+ <div>
+ 	<div class='header'>
+ 	  <div class='nav'>
+ 	    <nav>
+ 	    <ul>
+         <li class='header_text'><a href='index.html' class='nav_text'>JV</a></li>
+         <li class='header_text'>|</li>
+         <li class='headername_text'>Jess Vanam</li>
+         <li class='headerbar_text'>|</li>
+         <li class='header_text'><a href='projects.html' class='nav_text'>Projects</a></li>
+         <li class='header_text' class='nav_text'>|</li>
+         <li class='header_text'><a href='essays.html' class='nav_text'>Essays</a></li>
+         <li class='header_text' class='nav_text'>|</li>
+         <li class='header_text'><a href='art.html' class='nav_text'>Art</a></li>
+         <li class='header_text' class='nav_text'>|</li>
+         <li class='header_text'><a href='info.html' class='nav_text'>Info</a></li>
+ 	    </ul>
+ 	    </nav>
+ 	    <div><!--nav-->
+ 	  </div> <!--header-->
+ </div>
 <div id='info_title'>
   <p class=titular_text>Art</p>
 </div> <!-- info_title -->
@@ -51,7 +71,9 @@
   <p class='link'>Back (for a necklace)<br></p>
   <img class="sculpturephotofirst" src="images/backnecklace.jpg"/>
 </div> <!--contentunit-->
-
-<?php include("includes/footer.php"); ?>
+<div>
+	<p class='footer'>all media on this site, unless otherwise noted
+	&copy; 2017 Jessica Vanam </p>
+</div> <!--footer-->
  </body>
 </html>
