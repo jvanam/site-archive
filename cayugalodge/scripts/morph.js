95dcbbea627722b19fd7cd12014a15f442d50c9e
jvanam <jessicavanam@gmail.com>
2019-10-25 13:14:40 -0700

    Again

--- cayugalodge/scripts/morph.js
+++ cayugalodge/scripts/morph.js
@@ -7,8 +7,9 @@ var morphing = anime({
 });
 
 var nav = document.querySelector('#Layer_2');
+var touchnav = document.querySelector('.nav_svg');
 
 nav.addEventListener('mouseenter', function() { morphing.pause(); });
 nav.addEventListener('mouseleave', function() { morphing.play(); });
-nav.addEventListener('touchstart', function() { morphing.pause(); });
-nav.addEventListener('touchend', function() { morphing.play(); });
+touchnav.addEventListener('touchstart', function() { morphing.pause(); });
+touchnav.addEventListener('touchend', function() { morphing.play(); });
