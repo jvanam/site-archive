dfba431d513281fd9bf311a0f13b7544c487f194
jvanam <jessicavanam@gmail.com>
2019-10-25 13:04:36 -0700

    Touch attempt3

--- cayugalodge/scripts/morph.js
+++ cayugalodge/scripts/morph.js
@@ -7,8 +7,9 @@ var morphing = anime({
 });
 
 var nav = document.querySelector('#Layer_2');
+var body= document.querySelector('#indexBody');
 
 nav.addEventListener('mouseenter', function() { morphing.pause(); });
 nav.addEventListener('mouseleave', function() { morphing.play(); });
-nav.addEventListener('ontouchstart', function() { morphing.pause(); });
-nav.addEventListener('ontouchend', function() { morphing.pplay(); });
+body.addEventListener('touchstart', function() { morphing.pause(); });
+body.addEventListener('touchend', function() { morphing.pplay(); });
