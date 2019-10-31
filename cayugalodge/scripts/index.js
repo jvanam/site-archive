213fa0006929fabe7dff73ae2c93cf33408a2a0f
jvanam <jessicavanam@gmail.com>
2019-10-31 14:29:21 -0700

    Index touch events test

--- cayugalodge/scripts/index.js
+++ cayugalodge/scripts/index.js
@@ -7,7 +7,9 @@ var morphing = anime({
 });
 
 var nav = document.querySelector('.Layer_2');
-var touchnav = document.querySelector('#index-nav');
 
 nav.addEventListener('mouseenter', function() { morphing.pause(); });
 nav.addEventListener('mouseleave', function() { morphing.play(); });
+
+window.addEventListener('touchstart', function() { morphing.pause(); });
+window.addEventListener('touchend', function() { morphing.play(); });
