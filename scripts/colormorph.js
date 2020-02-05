c93c561352625d13d54df75dc7ce73eab06f7f34
jvanam <jessicavanam@gmail.com>
2020-02-05 11:10:01 -0800

    Updateee

---
+++ scripts/colormorph.js
@@ -0,0 +1,84 @@
+var color = anime ({
+  targets: '#abt-pic-1',
+  keyframes: [
+    {stroke: '#7c9df1'},
+    {stroke: '#5fc8b4'},
+    {stroke: '#b4e08b'},
+    {stroke: '#e7e496'},
+    {stroke: '#e7856a'},
+  ],
+  duration: 2500,
+  easing:'cubicBezier(.5, .05, .1, .3)',
+  direction: 'alternate',
+  loop: true,
+})
+var color = anime ({
+  targets: '#abt-pic-2',
+  keyframes: [
+    {stroke: '#5fc8b4'},
+    {stroke: '#b4e08b'},
+    {stroke: '#e7e496'},
+    {stroke: '#e7856a'},
+    {stroke: '#c9bdf3'},
+  ],
+  duration: 2500,
+  easing:'cubicBezier(.5, .05, .1, .3)',
+  direction: 'alternate',
+  loop: true,
+})
+var color = anime ({
+  targets: '#abt-pic-3',
+  keyframes: [
+    {stroke: '#b4e08b'},
+    {stroke: '#e7e496'},
+    {stroke: '#e7856a'},
+    {stroke: '#c9bdf3'},
+    {stroke: '#7c9df1'},
+  ],
+  duration: 2500,
+  easing:'cubicBezier(.5, .05, .1, .3)',
+  direction: 'alternate',
+  loop: true,
+})
+var color = anime ({
+  targets: '#abt-pic-4',
+  keyframes: [
+    {stroke: '#e7e496'},
+    {stroke: '#e7856a'},
+    {stroke: '#c9bdf3'},
+    {stroke: '#7c9df1'},
+    {stroke: '#5fc8b4'},
+  ],
+  duration: 2500,
+  easing:'cubicBezier(.5, .05, .1, .3)',
+  direction: 'alternate',
+  loop: true,
+})
+var color = anime ({
+  targets: '#abt-pic-5',
+  keyframes: [
+    {stroke: '#e7856a'},
+    {stroke: '#c9bdf3'},
+    {stroke: '#7c9df1'},
+    {stroke: '#5fc8b4'},
+    {stroke: '#b4e08b'},
+  ],
+  duration: 2500,
+  easing:'cubicBezier(.5, .05, .1, .3)',
+  direction: 'alternate',
+  loop: true,
+})
+var color = anime ({
+  targets: '#abt-pic-6',
+  keyframes: [
+    {stroke: '#c9bdf3'},
+    {stroke: '#7c9df1'},
+    {stroke: '#5fc8b4'},
+    {stroke: '#b4e08b'},
+    {stroke: '#e7e496'},
+  ],
+  duration: 2500,
+  easing:'cubicBezier(.5, .05, .1, .3)',
+  direction: 'alternate',
+  loop: true,
+})
